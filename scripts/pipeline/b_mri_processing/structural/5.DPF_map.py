e656f77501bb2d516e7f6beeb2afb77d29eb5006
Alex <alexandre.pron@gmail.com>
2020-05-10 13:42:13 +0200

    update : included DPF computation throught the brainvisa axon python api

--- scripts/pipeline/b_mri_processing/structural/5.DPF_map.py
+++ scripts/pipeline/b_mri_processing/structural/5.DPF_map.py
@@ -1,16 +1,21 @@
 """
 Compute Depth-Potential Function (DPF) maps from the BrainVISA python interface
+(more convenient) than command wrapping and direct python interface because it allows
+to use default set process values.
 """
-
-from configuration.configuration import SUBJ_LIST, SIDES
-
+import brainvisa.axon as axon
+import brainvisa.processes
+from configuration.configuration import SUBJ_LIST, SIDES, MESHES, DPFS
 
 if __name__ == '__main__':
 
+    # launch BrainVisa
+    axon.initializeProcesses()
+    context = brainvisa.processes.defaultContext()
     for i, subject in enumerate(SUBJ_LIST):
         for j, side in enumerate(SIDES):
+            context.runProcess('Depth Potential Function', input_mesh=MESHES[(subject, side)], DPF_texture=DPFS[(subject, side)])
             pass
-
-
+    axon.cleanup()
 
 
