fe243f1718f813aa35b0d9452697b674dc6b523a
Alex <alexandre.pron@gmail.com>
2020-05-18 11:03:35 +0200

    REFACTOR : cleaned path in cluster scripts

--- scripts/pipeline/f_projection/a_launch_extremities_extraction.py
+++ scripts/pipeline/f_projection/a_launch_extremities_extraction.py
@@ -3,32 +3,18 @@ import os
 
 if __name__ == '__main__':
 
-    from configuration.configuration import SUBJ_LIST
-    #metadata to lauch cluster
-    dir_cluster = os.path.join(DIR_PROJECT,'cluster','extremities')
+    from libs.tools.cluster import launch_subject_cmd
+    from configuration.configuration import SUBJ_LIST, SIDES, DIR_PROJECT, DIR_CLUSTER, ASSOCIATION_TRACTS, ASSO_TRACT_EXTREMITIES
+    dir_cluster = os.path.join(DIR_CLUSTER, 'extremities')
     if not os.path.exists(dir_cluster):
         os.makedirs(dir_cluster)
 
-
     for subject in SUBJ_LIST:
-
-
-        path_affine = os.path.join('/envau/work/meca/data/HCP/data/BV_database/subjects',sub,'dmri','default_acquisition',
-                                'HCP_pipeline','csd','MSMT', 'brain_fit','wm_fod_RAS_to_aims.npy')
-        if (os.path.exists(path_trk) and os.path.exists(path_affine)): #and not os.path.exists(os.path.join(DIR_OUT,'extremities', sub + '_s_points_0.npy')) :
-            path_s_points = os.path.join(DIR_OUT,'extremities', sub + '_s_points.npy')
-            path_e_points = os.path.join(DIR_OUT, 'extremities', sub + '_e_points.npy')
-
-            stderr = os.path.join(dir_cluster,sub + '.stderr')
-            stdout = os.path.join(dir_cluster, sub + '.stdout')
-            cmd_file = os.path.join(dir_cluster, sub + '.cmd')
-
+        for side in SIDES.keys():
+            # TODO : Find a way to have relative path
             cmd = os.path.join(DIR_PROJECT,'scripts','pipeline','d_extract_fibers_extremities','extract_streamlines_extremities_cluster.py') + '  ' + \
-                  path_trk + '  ' + path_affine + '  ' + path_s_points + '  ' + path_e_points
-            print  cmd
-            os.system('frioul_batch  -c 4 -d ' + dir_cluster + ' -O ' + stdout + ' -E ' + stderr + ' -C ' + cmd_file
-                      +  ' " ' + cmd + ' " ' )
-
+                  ASSOCIATION_TRACTS[(subject, side)] + '  ' + ASSO_TRACT_EXTREMITIES[(subject, side, 's')] + '  ' + ASSO_TRACT_EXTREMITIES[(subject, side, 'e')]
+            launch_subject_cmd(cmd, subject, dir_cluster)
 
 
 
--- scripts/pipeline/f_projection/b_launch_compute_nearest_vertex.py
+++ scripts/pipeline/f_projection/b_launch_compute_nearest_vertex.py
@@ -1,36 +1,24 @@
 import os
 import numpy as np
-
-
+from libs.tools.cluster import launch_subject_cmd
 
 
 if __name__ == '__main__':
 
+    from configuration.configuration import SUBJ_LIST, SIDES, DIR_PROJECT, DIR_CLUSTER, MESHES, TRACTS_EXTREMITIES, ASSO_TRACT_EXTREMITIES, ASSO_TRACT_NEAREST_VERTEX
 
-    dir_cluster = os.path.join(DIR_CLUSTER,'distance')
+    dir_cluster = os.path.join(DIR_CLUSTER, 'distance')
     if not os.path.exists(dir_cluster):
         os.makedirs(dir_cluster)
 
+    for subject in SUBJ_LIST:
+        for side in SIDES.keys():
+            for ext in TRACTS_EXTREMITIES:
+                if os.path.exists(ASSO_TRACT_EXTREMITIES):
+                        # TO DO find a way to have relative path so that it works without specifiying absolute path
+                        cmd = os.path.join(DIR_PROJECT, 'libs', 'projection', 'nearest_mesh_vertex_cluster.py') + '  ' + ASSO_TRACT_EXTREMITIES[(subject, side, ext)] + '  ' + MESHES[(subject, side, 'white')] + '  ' + ASSO_TRACT_NEAREST_VERTEX[(subject, side, ext)]
+                        launch_subject_cmd(cmd, subject + '_' + side, dir_cluster, core=8)
 
-
-    for sub in subjects:
-        path_vertices = os.path.join(DIR_OUT,'gw_interface', 'arrays', sub + '_wm_vertices.npy')
-        for ext in extremities:
-            path_points = os.path.join(DIR_OUT,'extremities', sub + '_'+ ext +'_points.npy')
-            path_index = os.path.join(DIR_OUT,'nearest_mesh_vertex', sub + '_' + ext + '_nearest_vertex.npy')
-            
-            if os.path.exists(path_points):
-                    stderr = os.path.join(dir_cluster,sub + '_' + ext  +'points.stderr')
-                    stdout = os.path.join(dir_cluster,sub + '_' + ext  +'points.stdout')
-                    cmd_file = os.path.join(dir_cluster,sub + '_' + ext  +'points.cmd')
-                    cluster_files = [stderr,stdout,cmd_file]
-                    for c in cluster_files:
-                            if os.path.exists(c):
-                                    os.remove(c)
-
-                    cmd = os.path.join(DIR_PROJECT,'scripts','pipeline','e_nearest_mesh_vertex', 'f_compute_nearest_vertex.py') + '  '  + path_points + '  ' + path_vertices + '  ' + path_index
-                    #print cmd
-                    os.system('frioul_batch  -c  8 -d ' + dir_cluster + ' -O ' + stdout + ' -E ' + stderr + ' -C ' + cmd_file +  ' " ' + cmd + ' " ' )
 
 
 
