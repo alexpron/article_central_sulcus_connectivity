5b5a9897cf54c3bcc38d1d6bbfe48c9f583c834a
Alex <alexandre.pron@gmail.com>
2020-05-18 14:05:16 +0200

    REFACTOR : removed path in association streamlines and ppfm scripts

--- scripts/pipeline/d_tractogram_filtering/c_association_streamlines.py
+++ scripts/pipeline/d_tractogram_filtering/c_association_streamlines.py
@@ -36,7 +36,13 @@ def select_association_streamline(path_mask, path_affine_dwi_to_t1, path_tractog
 
 if __name__ == '__main__':
 
-    from configuration.configuration import TRACTS
+    from configuration.configuration import SUBJ_LIST, SIDES, DWI_2_T1,TISSUE_MASKS, TRACTS, ASSOCIATION_TRACTS
+
+    for subject in SUBJ_LIST:
+        for side in SIDES.keys():
+            select_association_streamline(TISSUE_MASKS[(subject, side)], DWI_2_T1[subject], TRACTS[(subject, 'trk', 'filtered')], ASSOCIATION_TRACTS[(subject, side)])
+
+
 
 
 
--- scripts/pipeline/e_drawing_landmarks/e_ppfm/d_PP_coord_on_gyrus.py
+++ scripts/pipeline/e_drawing_landmarks/e_ppfm/d_PP_coord_on_gyrus.py
@@ -1,19 +1,14 @@
 """
-
+Retrieve the coordinate of the PPFM along the crest lines according to the chosen parametrisation
 """
 
-import os
 import numpy as np
 import pandas as pd
-from configuration import DIR_IN, DIR_OUT, SUBJ_LIST, SIDES, GYRI
-
-
+from configuration.configuration import SUBJ_LIST, SIDES, GYRI, PPFM_TABLES, GYRAL_PARAMETRISATIONS
 
 if __name__ == '__main__':
 
-    path_df = os.path.join(DIR_OUT,'pli_passage','tables','pp_manual_drawing_index_gyri.csv')
-    path_df_out = os.path.join(DIR_OUT,'pli_passage','tables','pp_manual_drawing_coord_gyri.csv')
-    df = pd.read_csv(path_df)
+    df = pd.read_csv(PPFM_TABLES['gyri_index'])
     df['PP_Pre_Coord_Iso'] = -1
     df['PP_Post_Coord_Iso'] = -1
     for i, subject in enumerate(SUBJ_LIST):
@@ -23,17 +18,8 @@ if __name__ == '__main__':
                 gyr_suffix = gyr_suffix.upper()[0] + gyr_suffix[1:]
                 variable_in = 'PP' + '_' + gyr_suffix + '_' + 'Index_Line'
                 variable_out = 'PP' + '_' + gyr_suffix + '_' + 'Coord_Iso'
-                path_gyrus_param = os.path.join(DIR_OUT,'gyri', subject + '_' + side + '_' + gyrus + '_' + 'cleaned'
-                                                + '_' +
-                                                'iso_param' + '.npy')
-                pp_index_gyrus = int(df.loc[(df['Subject']==int(subject)) & (df['Hemisphere']==side), variable_in])
-                gyrus_param = np.load(path_gyrus_param)
+                pp_index_gyrus = int(df.loc[(df['Subject'] == int(subject)) & (df['Hemisphere'] == side), variable_in])
+                gyrus_param = np.load(GYRAL_PARAMETRISATIONS[(subject, side, gyrus,'cleaned','array','iso')])
                 pp_coord = gyrus_param[pp_index_gyrus]
                 df.loc[(df['Subject'] == int(subject)) & (df['Hemisphere'] == side), variable_out] = pp_coord
-
-    df.to_csv(path_df_out)
-
-
-
-
-
+    df.to_csv(PPFM_TABLES['gyri_coord'])
