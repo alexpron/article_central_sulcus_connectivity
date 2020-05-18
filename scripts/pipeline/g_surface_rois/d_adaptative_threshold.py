15fa35581d47ee0d0acac67c88d0d6143a9d6d29
Alex <alexandre.pron@gmail.com>
2020-05-18 14:41:08 +0200

    REFACTOR : cleaned path in the U-fibers scripts

--- scripts/pipeline/g_surface_rois/d_adaptative_threshold.py
+++ scripts/pipeline/g_surface_rois/d_adaptative_threshold.py
@@ -10,17 +10,17 @@ if __name__ == '__main__':
         for j, side in enumerate(SIDES):
             mesh = aims.read(MESHES[(subject, side, 'white')])
             for k, gyrus in enumerate(GYRI):
-                #Gyral line indexes
+                # Gyral line indexes
                 gyral_line = np.load(GYRAL_CRESTS[(subject, side, 'cleaned', 'array')])
                 gyral_line = gyral_line.tolist()
-                #Geodesic distance map
+                # Geodesic distance map
                 geo_dist_t = aims.read(GEO_DISTS[(subject, side, 'white', gyrus)])
                 geo_dist = np.array(geo_dist_t[0])
-                #Mesh partitionned by geodesic distance
+                # Mesh partitionned by geodesic distance
                 partition = np.load(PARTITIONS[(subject, side, gyrus, 'array')])
-                #Varying distance threshold
+                # Varying distance threshold
                 dist_threshold = np.load(ROI_DISTANCES[(subject, side, gyrus)])
-                #Output texture
+                # Output texture
                 roi = np.zeros(len(geo_dist),dtype=np.uint32)
                 for l, g in enumerate(gyral_line):
                     roi[(partition == g)*(geo_dist <= dist_threshold[l])] = 1
