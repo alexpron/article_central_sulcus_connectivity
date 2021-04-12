807e223658a115392b92c61e024484fd32f919d4
alexpron <alexandre.pron@gmail.com>
2021-04-12 23:04:51 +0200

    BF: fixe path names organisation on INT cluster

--- fix_orga.py
+++ fix_orga.py
@@ -3,10 +3,9 @@ import glob
 
 
 dir_meshes = '/hpc/meca/users/pron.a/data/U_Fibers/meshes_and_textures'
-wrong_typo_files = glob.glob(os.path.join(dir_meshes, '*[LR]white*'))
+wrong_typo_files = glob.glob(os.path.join(dir_meshes, '*DPF*'))
 for f in wrong_typo_files:
     filename = os.path.basename(f)
-    t = filename.replace('Lwhite','L_white')
-    s = t.replace('Rwhite','R_white')
-    gf = os.path.join(dir_meshes, s)
+    t = filename.replace('DPF','dpf')
+    gf = os.path.join(dir_meshes, t)
     os.rename(f, gf)
