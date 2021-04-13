0095ec5ef343e7dce6ed893a31efd157a91b5e15
alexpron <alexandre.pron@gmail.com>
2021-04-13 11:51:06 +0200

    BF: fixe path names organisation on INT cluster: geodesic distances map

--- fix_orga.py
+++ fix_orga.py
@@ -6,7 +6,6 @@ dir_meshes = '/hpc/meca/users/pron.a/data/U_Fibers/sulci/cs/landmarks/surf_rois/
 wrong_typo_files = glob.glob(os.path.join(dir_meshes, '*.npy'))
 for f in wrong_typo_files:
     filename = os.path.basename(f)
-    t = filename.replace('.npy', '')
-    u = t + '"dist_threshold.npy'
-    gf = os.path.join(dir_meshes, u)
+    t = filename.replace('"', '_')
+    gf = os.path.join(dir_meshes, t)
     os.rename(f, gf)
