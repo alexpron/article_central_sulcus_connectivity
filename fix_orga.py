d17660f1f7cc63dfdd93558a9395bde839cdb85b
alexpron <alexandre.pron@gmail.com>
2021-04-13 08:05:01 +0200

    BF: fixe path names organisation on INT cluster

--- fix_orga.py
+++ fix_orga.py
@@ -2,10 +2,10 @@ import os
 import glob
 
 
-dir_meshes = '/hpc/meca/users/pron.a/data/U_Fibers/sulci/cs/landmarks/fundus/cleaned'
-wrong_typo_files = glob.glob(os.path.join(dir_meshes, '*.npy'))
+dir_meshes = '/hpc/meca/users/pron.a/data/U_Fibers/sulci/cs/landmarks/adjacent_gyri'
+wrong_typo_files = glob.glob(os.path.join(dir_meshes, '*.gii'))
 for f in wrong_typo_files:
     filename = os.path.basename(f)
-    t = filename.replace('central_sulcus','cs')
+    t = filename.replace('.tex','')
     gf = os.path.join(dir_meshes, t)
     os.rename(f, gf)
